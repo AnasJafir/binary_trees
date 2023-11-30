574ee4bf9a1ca0c4b4cf036dd51022560572494c
AnasJafir <dr.anas.jafir@gmail.com>
2023-11-30 14:30:51 -0800

    measuring the leaves of a binary tree

---
+++ 12-binary_tree_leaves.c
@@ -0,0 +1,34 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_is_leaf - checks if a node is a leaf node
+ * @node: node to check
+ * Return: 1 if it's a leaf
+ *          0 otherwise
+ *          0 if node is NULL
+*/
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+        if (node == NULL)
+                return (0);
+        if (node->left == NULL && node->right == NULL)
+                return (1);
+        return (0);
+}
+/**
+ * binary_tree_leaves - counts the leaves in a binary tree
+ * @tree: pointer to the root node of the tree to count the number of leaves
+ * Return: the leaves count otherwise 0
+*/
+int binary_tree_is_leaf(const binary_tree_t *node);
+size_t binary_tree_leaves(const binary_tree_t *tree)
+{
+	size_t count = 0;
+
+	if (tree == NULL)
+		return (0);
+	if (binary_tree_is_leaf(tree))
+		return (1);
+	count += binary_tree_leaves(tree->left);
+	count += binary_tree_leaves(tree->right);
+	return (count);
+}
